f9ea353a57fcc2514a0d844a46c7d04f3ef9c2c3
Tan Dung <tandung29022004@gmail.com>
2025-03-12 14:08:50 +0700

    update

---
+++ BaiTapThuyetTrinh/Controllers/LanguageController.cs
@@ -0,0 +1,31 @@
+﻿using Microsoft.AspNetCore.Localization;
+using Microsoft.AspNetCore.Mvc;
+
+namespace BaiTapThuyetTrinh.Controllers
+{
+    public class LanguageController : Controller
+    {
+        [HttpPost]
+        public IActionResult SetLanguage(string culture, string returnUrl)
+        {
+            if (!string.IsNullOrEmpty(culture))
+            {
+                Response.Cookies.Append(
+                    CookieRequestCultureProvider.DefaultCookieName,
+                    CookieRequestCultureProvider.MakeCookieValue(new RequestCulture(culture)),
+                    new CookieOptions { Expires = DateTimeOffset.UtcNow.AddYears(1) }
+                );
+            }
+
+            return LocalRedirect(returnUrl ?? "/");
+        }
+
+
+        public IActionResult Index()
+        {
+            var culture = HttpContext.Features.Get<IRequestCultureFeature>()?.RequestCulture.Culture.Name ?? "en-US";
+            ViewData["Culture"] = culture;
+            return View();
+        }
+    }
+}
---
+++ BaiTapThuyetTrinh/Program.cs
@@ -0,0 +1,62 @@
+﻿using Microsoft.AspNetCore.Localization;
+using System.Globalization;
+using Microsoft.Extensions.Options;
+
+var builder = WebApplication.CreateBuilder(args);
+
+// Add services to the container.
+builder.Services.AddControllersWithViews();
+
+//Thêm dịch vụ đa ngôn ngữ
+builder.Services.AddLocalization(options => options.ResourcesPath = "Resources");
+builder.Services.AddSingleton<IHttpContextAccessor, HttpContextAccessor>();
+
+//Thêm MVC và hỗ trợ ViewLocalizer
+builder.Services.AddControllersWithViews()
+    .AddViewLocalization()  // Bổ sung hỗ trợ IViewLocalizer
+    .AddDataAnnotationsLocalization();
+
+// Cấu hình đa ngôn ngữ
+builder.Services.Configure<RequestLocalizationOptions>(options =>
+{
+    var supportedCultures = new[]
+    {
+        new CultureInfo("vi-VN"),  // Tiếng Việt (mặc định)
+        new CultureInfo("en-US"),  // Tiếng Anh
+        new CultureInfo("fr-FR")   // Tiếng Pháp (nếu cần)
+    };
+
+    options.DefaultRequestCulture = new RequestCulture("vi-VN");
+    options.SupportedCultures = supportedCultures;
+    options.SupportedUICultures = supportedCultures;
+});
+
+
+var app = builder.Build();
+// Áp dụng Localization Middleware
+var locOptions = app.Services.GetRequiredService<IOptions<RequestLocalizationOptions>>().Value;
+app.UseRequestLocalization(locOptions);
+
+
+// Configure the HTTP request pipeline.
+if (!app.Environment.IsDevelopment())
+{
+    app.UseExceptionHandler("/Home/Error");
+    // The default HSTS value is 30 days. You may want to change this for production scenarios, see https://aka.ms/aspnetcore-hsts.
+    app.UseHsts();
+}
+
+app.UseHttpsRedirection();
+app.UseRouting();
+
+app.UseAuthorization();
+
+app.MapStaticAssets();
+
+app.MapControllerRoute(
+    name: "default",
+    pattern: "{controller=Home}/{action=Index}/{id?}")
+    .WithStaticAssets();
+
+
+app.Run();
